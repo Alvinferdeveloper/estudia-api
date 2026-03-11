dbe8c5e6a9902e5d458d1358759c17b9031c5d90
Albin <alvinfer67@gmail.com>
2026-03-10 21:21:26 -0600

    feat: implement email and password register

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,10 +6,22 @@ export const auth = betterAuth({
     database: typeormAdapter(dataSource),
     basePath: '/api/auth',
     trustedOrigins: ['http://localhost:3000'],
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: true,
+    },
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+        facebook: {
+            clientId: process.env.FACEBOOK_CLIENT_ID as string,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
         },
     },
 });
